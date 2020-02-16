1eb7170dce2f0ff652fff4df95e58e5f68386a8c
Jeremy Bonte <jbonte@colgate.edu>
2020-02-15 19:55:12 -0500

    Testing subnet

--- subnetting.py
+++ subnetting.py
@@ -2,16 +2,26 @@ import ipaddress
 import random
 
 def main():
-    ip1 = ipaddress.IPv4Address("10.0.5.234")
-    test_size_subnet()
+    n1 = ipaddress.ip_network('5.0.5.234/24')
+    print(n1)
+    #test_size_subnet()
+    test_subnet()
 
 def test_size_subnet():
     rand = random.randint(0,32)
     num_hosts = 2 ** rand
     print("What size subnet can accommodate up to %d hosts?"%num_hosts)
-    print("Fomat your answer in: \'--/{answer}\'")
-    user_answer = input("Answer: ")
-    if (user_answer == str(rand)):
+    while(True):
+        user_answer = input("Answer: ").strip()
+        try:
+            response = int(user_answer)
+            if(response < 0 or response > 32):
+                print("Number must be within range 0-32")
+                continue
+            break
+        except Exception:
+            print("PLEASE ENTER AN INTEGER")
+    if (response == rand):
         print("Correct!")
     else:
         print("Incorrect! The correct answer was %d"%rand)
@@ -20,6 +30,15 @@ def test_ip_address_to_network():
     return None
 
 def test_subnet():
+    addr1 = random.randint(0,255)
+    addr2 = random.randint(0,255)
+    rand3N = random.randint(0,255)
+    rand3A = random.randint(0,255)
+    rand4N = random.randint(0,255)
+    rand4A = random.randint(0,255)
+    host_bitsN = random.randint(0,32)
+    host_bitsA = random.randint(0,32)
+    network =
     print()
 
 def number_of_hosts(num):
