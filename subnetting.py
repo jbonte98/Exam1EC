a375d6a18c2e1d14fa11c59f3835654980c7060c
Jeremy Bonte <jbonte@colgate.edu>
2020-02-17 09:50:30 -0500

    Final commit

--- subnetting.py
+++ subnetting.py
@@ -2,15 +2,15 @@ import ipaddress
 import random
 
 def main():
-    n1 = ipaddress.ip_network('5.0.5.234/24')
-    print(n1)
     #test_size_subnet()
-    test_subnet()
+    #test_subnet()
+    print(next(ipaddress.ip_network("10.0.5.0/24").hosts()))
+    test_address_range()
 
 def test_size_subnet():
     rand = random.randint(0,32)
     num_hosts = 2 ** rand
-    print("What size subnet can accommodate up to %d hosts?"%num_hosts)
+    print("What size subnet(hostmask) can accommodate up to %d hosts?"%num_hosts)
     while(True):
         user_answer = input("Answer: ").strip()
         try:
@@ -27,22 +27,60 @@ def test_size_subnet():
         print("Incorrect! The correct answer was %d"%rand)
 
 def test_ip_address_to_network():
+    while True:
+        try:
+            guess = input("Print as a properly formated address [0:255].[0:255].[0:255].[0:255]/[0:32]")
+            guess_address = ipaddress.ip_address(guess)
+            break
+        except Exception:
+            print("NOT A VALID IP ADDRESS")
     return None
+
+def test_address_range():
+    addr1 = random.randint(0,255)
+    addr2 = random.randint(0,255)
+    addr3 = random.randint(0,255)
+    addr4 = random.randint(0,255)
+    host_bits = random.randint(0,32)
+    network_string = "%d.%d.%d.%d/%d"%(addr1,addr2,addr3,addr4,host_bits)
+    network = ipaddress.ip_network(network_string,strict=False)
+    print("What is the range of addresses in the subnet %s?"%(network))
+    lowest = input("Lowest Address?")
+    highest = input("Highest Address?")
+    try:
+        lowest_address = ipaddress.ip_address(lowest)
+        highest_address = ipaddress.ip_address(highest)
+    except Exception:
+        print("One of the addresses was not properly formatted!")
+    if(lowest == network and highest_address in network.hosts()):
+        print("Correct!!!!")
+    else:
+        print("Wrong!!! Correct answer was %s through %s"%(network,"5343"))
 
 def test_subnet():
     addr1 = random.randint(0,255)
     addr2 = random.randint(0,255)
-    rand3N = random.randint(0,255)
-    rand3A = random.randint(0,255)
-    rand4N = random.randint(0,255)
-    rand4A = random.randint(0,255)
+    addr3N = random.randint(0,255)
+    addr3A = random.randint(0,255)
+    addr4N = random.randint(0,255)
+    addr4A = random.randint(0,255)
     host_bitsN = random.randint(0,32)
-    host_bitsA = random.randint(0,32)
-    network =
-    print()
-
-def number_of_hosts(num):
-    return "{0:b}".format(num)
+    host_bitsA = random.randint(0,(host_bitsN+1))
+    network_string = "%d.%d.%d.%d/%d"%(addr1,addr2,addr3N,addr4N,host_bitsN)
+    network = ipaddress.ip_network(network_string,strict=False)
+    address = ipaddress.ip_network("%d.%d.%d.%d/%d"%(addr1,addr2,addr3A,addr4A))
+    answer = address in network.subnets()
+    print("Is %s/%d a subnet of %s?"%(address,host_bitsA,network_string))
+    while True:
+        guess = input("True or False: ")
+        if(guess == "True" or guess == "False"):
+            break
+        else:
+            print("Not in the form True/False")
+    if (str(answer) == guess):
+        print("Correct!!!")
+    else:
+        print("Wrong!!! The correct answer was %s"%(str(answer)))
 
 if __name__ == '__main__':
     main()
